1f57263903e80313b3ed3acf0443e1e8e998ab05
Lucas Falcao <lucas.falcao.nb@gmail.com>
2023-05-24 16:18:38 -0300

    Login screen base layout.

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import { MyTextInput } from '../../components/MyTextInput';
+import { TextHeader } from '../../components/TextHeader';
+import { MyButton } from '../../components/MyButton';
+import { TextLabel } from '../../components/TextLabel';
+import { Spacer } from '../../components/Spacer';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { TextStyle, ViewStyle } from 'react-native';
+import { MyPasswordInput } from '../../components/MyPasswordInput';
 
 
 const LoginScreen: React.FC = () => {
   return (
-    null
+    <SafeAreaView style={containerStyle}>
+      <TextHeader style={centerText}>Login</TextHeader>
+
+      <Spacer size={38}/>
+
+      <MyTextInput
+        label='E-mail'>  
+      </MyTextInput>
+
+      <Spacer size={20}/>
+
+      <MyPasswordInput
+        label='Password'
+        placeholder='Minimum 8 characters'
+        value='testtestetestetesttetstetesttesttesttestetesttetetstetsttesttesttesttteststtest'>
+      </MyPasswordInput>
+
+      <Spacer size={38}/>
+
+      <MyButton label='Login'/>
+
+      <Spacer size={12}/>
+      
+      <TextLabel style={centerText}>
+        Don’t have an account? Sign up here
+      </TextLabel>
+    </SafeAreaView>
   )
+}
+
+const containerStyle: ViewStyle = {
+  padding: 20,
+}
+
+const centerText: TextStyle = {
+  textAlign: 'center',
 }
 
 export default LoginScreen;
