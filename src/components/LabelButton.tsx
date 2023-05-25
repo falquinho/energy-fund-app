8a96153db506ee64fc8a94020f128c101e50c36a
Lucas Falcao <lucas.falcao.nb@gmail.com>
2023-05-24 21:58:37 -0300

    LabelButton refactor: now doesn't rely on TouchableOpacity.

--- src/components/LabelButton.tsx
+++ src/components/LabelButton.tsx
@@ -1,23 +1,16 @@
 import React from 'react'
-import { TextStyle, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { TextProps, TextStyle, TouchableOpacity, TouchableOpacityProps, ViewStyle } from 'react-native'
 import { TextLabel } from './TextLabel'
 import { useMyTheme } from '../navigation/theme'
 import { ThemeColors } from '../navigation/theme.types'
 
 
-export type LabelButtonProps = TouchableOpacityProps & {
-  label: string,
-}
 
-export const LabelButton: React.FC<LabelButtonProps> = (props) => {
+export const LabelButton: React.FC<TextProps> = (props) => {
   const { colors } = useMyTheme();
   
   return (
-    <TouchableOpacity {...props}>
-      <TextLabel style={textStyle(colors)}>
-        {props.label}
-      </TextLabel>
-    </TouchableOpacity>
+    <TextLabel {...props} style={[textStyle(colors), props.style]}/>
   )
 }
 
