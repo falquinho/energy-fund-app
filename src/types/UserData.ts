dea607769c83d563628131a6e0b6f6b4fe317805
Lucas Falcao <lucas.falcao.nb@gmail.com>
2023-05-26 18:05:59 -0300

    Add email field to UserData.

--- src/types/UserData.ts
+++ src/types/UserData.ts
@@ -1,6 +1,8 @@
 import { UserFund } from "./UserFund"
 
 export type UserData = {
+  id: number,
+  email: string,
   total_balance: number,
   variation: number,
   user_portfolios: Array<UserFund>
