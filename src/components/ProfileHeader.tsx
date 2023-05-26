9c3bea462697ce800578ce90f08c8fb5b3843348
Lucas Falcao <lucas.falcao.nb@gmail.com>
2023-05-26 16:06:17 -0300

    Finished ProfileHeader layout and use data from redux store.

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -1,33 +1,83 @@
 import React from 'react';
-import { TextStyle, View, ViewStyle } from 'react-native';
+import { StyleSheet, TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { useMyTheme } from '../navigation/theme';
+import { ThemeColors } from '../navigation/theme.types';
+import { GrowthTag } from './GrowthTag';
 import { HeaderContainer } from './HeaderContainer';
 import { IconButton } from './IconButton';
-import { TextHeader } from './TextHeader';
-import { ThemeColors } from '../navigation/theme.types';
-import { useMyTheme } from '../navigation/theme';
 import { Row } from './Row';
+import { Spacer } from './Spacer';
+import { Text } from './Text';
+import { TextContrast } from './TextContrast';
+import { TextHeader } from './TextHeader';
+import { useReduxSelector } from '../redux/hooks';
+import { selectUserData } from '../redux/users.slice';
 
 
 export const ProfileHeader: React.FC = () => {
+  const userData = useReduxSelector(selectUserData);
   const { colors } = useMyTheme();
   const iconStyle = buildIconStyle(colors);
+
+  if (!userData)
+    return <HeaderContainer/>;
+
   return (
-    <HeaderContainer style={row}>
-      <IconButton iconName='person-circle-outline' iconStyle={iconStyle}/>
+    <HeaderContainer>
       <Row style={row}>
-        <TextHeader>Account: $1,457.23</TextHeader>
-        <Icon name='chevron-down'/>
+        <IconButton iconName='person-circle-outline' iconStyle={iconStyle}/>
+        <Row style={row}>
+          <TextHeader>Account: ${userData.total_balance}</TextHeader>
+          <Icon name='chevron-down'/>
+        </Row>
+        <IconButton iconName='notifications-outline' iconStyle={iconStyle}/>
       </Row>
-      <IconButton iconName='notifications-outline' iconStyle={iconStyle}/>
+      
+      <Spacer size={20}/>
+
+      <Text style={{ fontSize: 12, marginBottom: 6 }}>Portfolio</Text>
+      <Row style={row}>
+        <Row style={{ alignItems: 'flex-end' }}>
+          <TextHeader style={valueText}>
+            ${userData.total_balance}
+          </TextHeader>
+          <GrowthTag growth={userData.variation}/>
+        </Row>
+
+        <TouchableOpacity style={touchable} onPress={() => {}}>
+          <View style={[backdrop(colors)]}></View>
+          <TextContrast outline>
+            <Icon name='trophy-outline'/> Earn Rewards
+          </TextContrast>
+        </TouchableOpacity>
+      </Row>
     </HeaderContainer>
   )
 }
 
 const row: ViewStyle = {
-  alignItems: 'center',
   justifyContent: 'space-between',
+  alignItems: 'center',
 }
+
+const valueText: TextStyle = {
+  fontSize: 24,
+  lineHeight: undefined,
+  marginRight: 4,
+}
+
+const touchable: ViewStyle = {
+  borderRadius: 4,
+  overflow: 'hidden',
+  padding: 8,
+}
+
+const backdrop = (colors: ThemeColors): ViewStyle => ({
+  ...StyleSheet.absoluteFillObject,
+  opacity: 0.1,
+  backgroundColor: colors.primary,
+})
 
 const buildIconStyle = (colors: ThemeColors): TextStyle => ({
   fontSize: 24,
