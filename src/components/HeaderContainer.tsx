d37ccec413fee684b6f4cd970415601fbdfa81b5
Lucas Falcao <lucas.falcao.nb@gmail.com>
2023-05-26 16:05:49 -0300

    Adjust HeaderContainer style to add bottom border.

--- src/components/HeaderContainer.tsx
+++ src/components/HeaderContainer.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { View, ViewProps, ViewStyle } from 'react-native'
 import { EdgeInsets, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useMyTheme } from '../navigation/theme';
+import { ThemeColors } from '../navigation/theme.types';
 
 
 /**
@@ -9,16 +11,20 @@ import { EdgeInsets, useSafeAreaInsets } from 'react-native-safe-area-context';
  * @returns 
  */
 export const HeaderContainer: React.FC<ViewProps> = (props) => {
+  const { colors } = useMyTheme();
   const insets = useSafeAreaInsets();
 
   return (
-    <View {...props} style={[paddings(insets), props.style]} />
+    <View {...props} style={[baseStyle(colors, insets), props.style]} />
   )
 }
 
-const paddings = (insets: EdgeInsets): ViewStyle => ({
+
+const baseStyle = (colors: ThemeColors, insets: EdgeInsets): ViewStyle => ({
   paddingLeft: 20 + insets.left,
   paddingRight: 20 + insets.right,
   paddingTop: 20 + insets.top,
   paddingBottom: 20,
+  borderBottomWidth: 1,
+  borderBottomColor: colors.grayF4
 })
