0ec87bbfc427c3955cb84828b1faee5b053c9cec
Lucas Falcao <lucas.falcao.nb@gmail.com>
2023-05-27 16:11:33 -0300

    Update onboarding navigator with new headers.

--- src/navigation/onboardNavigator.tsx
+++ src/navigation/onboardNavigator.tsx
@@ -2,6 +2,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/login";
 import SignupScreen from "../screens/signup";
 import { LoggedInNavigator } from "./loggedInNavigator";
+import { HeaderContainer } from "../components/HeaderContainer";
+import { HeaderUnlabel } from "../components/HeaderUnlabeled";
 
 
 export type OnboardingStackParamList = {
@@ -14,8 +16,22 @@ const Stack = createNativeStackNavigator<OnboardingStackParamList>();
 
 export const OnboardingStack: React.FC = () => (
   <Stack.Navigator>
-    <Stack.Screen name="Login"  component={LoginScreen}/>
-    <Stack.Screen name="SignUp" component={SignupScreen}/>
-    <Stack.Screen name="Main" component={LoggedInNavigator} options={{headerShown: false}}/>
+    <Stack.Screen
+      name="Login"
+      component={LoginScreen}
+      options={{ header: () => <HeaderContainer/>}}
+    />
+    <Stack.Screen
+      name="SignUp"
+      component={SignupScreen}
+      options={{
+        header: ({ navigation }) => <HeaderUnlabel navigation={navigation}/>
+      }}
+    />
+    <Stack.Screen
+      name="Main"
+      component={LoggedInNavigator}
+      options={{ headerShown: false }}
+    />
   </Stack.Navigator>
 )
