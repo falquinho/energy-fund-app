6fcbc1c463a8d9c2edbcae27c64f34071ead5c47
Lucas Falcao <lucas.falcao.nb@gmail.com>
2023-05-28 22:38:11 -0300

    Fix Text on android by using a mapper from fontWeight to fontFamily.

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from '@react-navigation/native'
 import React from 'react'
-import { TextProps, Text as ReactText } from 'react-native';
+import { TextProps, Text as ReactText, TextStyle, StyleSheet } from 'react-native';
 
 
 /**
@@ -11,14 +11,35 @@ import { TextProps, Text as ReactText } from 'react-native';
  */
 export const Text: React.FC<TextProps> = (props) => {
   const { colors } = useTheme();
+
+  const flattenedStyle: TextStyle = StyleSheet.flatten(props.style);
+
+  const familyStyle: TextStyle = { 
+    fontFamily: weigthToFamilyMap[''+ flattenedStyle.fontWeight],
+  }
+
   return (
     <ReactText
       {...props}
       style={[
         { color: colors.text },
-        { fontFamily: 'Sora' },
-        props.style
+        props.style,
+        familyStyle,
       ]}
     />
   );
+}
+
+const weigthToFamilyMap: Record<string, string> = {
+  '100': 'Sora-Thin',
+  '200': 'Sora-ExtraLight',
+  '300': 'Sora-Light',
+  '400': 'Sora-Regular',
+  '500': 'Sora-Medium',
+  '600': 'Sora-SemiBold',
+  '700': 'Sora-Bold',
+  '800': 'Sora-ExtraBold',
+  '900': 'Sora-ExtraBold',
+  'normal': 'Sora-Regular',
+  'bold': 'Sora-Bold',
 }
